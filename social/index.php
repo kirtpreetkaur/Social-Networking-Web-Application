a722c90e99003e0a8b470a6600e57e8593831726
KIRTPREET KAUR <84916234+kirtpreetkaur@users.noreply.github.com>
2023-09-26 12:27:53 +0530

    website

---
+++ social/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Welcome  To Bio-Bbook - Sin up, Log in, Chat </title>
+		<link rel="stylesheet" type="text/css" href="css/index.css">
+		<style>
+			h1 {
+				text-align:center;
+				margin-top: 1.4em;
+				font-size: 10em;
+				color:#000080;
+  				font-weight: bold;
+			}
+		</style>
+	</head>
+
+<body>
+
+	<div id="header">
+		<div class="head-view">
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li><a href="signin.php" title="Sign in"><button class="btn-sign-in" value="Sign in">Sign in</button></a></li>
+				<li><a href="signup.php" title="Sign up"><button class="btn-sign-up" value="Sign up">Sign up</button></a></li>
+			</ul>
+		</div>
+	</div>
+
+	<div id="container">
+			<h1>BIO-BOOK</h1>
+	</div>
+
+</body>
+
+</html>
